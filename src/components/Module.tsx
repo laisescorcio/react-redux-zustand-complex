80929e4002626116212d88df603e4c0cb781f24d
Laís Escorcio <61030205+laisescorcio@users.noreply.github.com>
2024-07-10 07:48:51 -0300

    feat: define props os Lessons and Module components

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -1,24 +1,30 @@
 import { ChevronDown, Video } from "lucide-react";
 import { Lesson } from "./Lesson";
 
-export function Module() {
+interface ModuleProps {
+  moduleIndex: number;
+  title: string;
+  amountOfLessons: number;
+}
+
+export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
   return (
     <div className="">
       <button className="flex w-full items-center gap-3 bg-zinc-800 p-4">
         <div className="flex h-10 w-10 rounded-full items-center justify-center bg-zinc-950 text-xs">
-          1
+          {moduleIndex + 1}
         </div>
         <div className="flex flex-col gap-1 text-left">
-          <strong className="text-sm">Desvendando o Redux</strong>
-          <span className="text-xs text-zinc-400">12 aulas</span>
+          <strong className="text-sm">{title}</strong>
+          <span className="text-xs text-zinc-400">{amountOfLessons}</span>
         </div>
 
         <ChevronDown className="w-5 h-5 ml-auto text-zinc-400" />
       </button>
       <nav className="relative flex flex-col gap-4 p-6">
-        <Lesson />
-        <Lesson />
-        <Lesson />
+        <Lesson title="Fundamentos do Redux" duration="09:13" />
+        <Lesson title="Fundamentos do Redux" duration="09:13" />
+        <Lesson title="Fundamentos do Redux" duration="09:13" />
       </nav>
     </div>
   );
